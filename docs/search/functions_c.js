2f3643daf514376ca4da4337e9128dd18f21a2cf
Eric Klavins <klavins@uw.edu>
2019-03-11 17:54:25 -0700

    example project directory structure

--- docs/search/functions_c.js
+++ docs/search/functions_c.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['reset',['reset',['../classelma_1_1Event.html#a4d9c5f6fa031752cde8c03b6dbbbd36a',1,'elma::Event']]],
-  ['run',['run',['../classelma_1_1Manager.html#ad5df7316416ac9697d62e68e21f04758',1,'elma::Manager']]]
+  ['run',['run',['../classelma_1_1Manager.html#ad5df7316416ac9697d62e68e21f04758',1,'elma::Manager::run(high_resolution_clock::duration)'],['../classelma_1_1Manager.html#a5ed2c14e0ba2d7243f33ba3b47c01ad5',1,'elma::Manager::run()']]]
 ];
